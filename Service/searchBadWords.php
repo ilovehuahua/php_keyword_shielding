5b24fe30c436a901d0f4b516cc3d798afb5b5bba
xun_yu <xun_yu@hunliji.com>
2018-12-27 15:15:11 +0800

    修正取代算法，优化效率

--- Service/searchBadWords.php
+++ Service/searchBadWords.php
@@ -61,7 +61,7 @@ class searchBadWords {
         $len = mb_strlen($string);
         $tmp = $this->map;
         $map = $this->map;
-        $out=$string;
+        $outString='';
         $str = '';
         for ($i = 0; $i < $len; $i++) {
             $nowWord = mb_substr($string, $i, 1);
@@ -70,12 +70,8 @@ class searchBadWords {
                 $str .= $nowWord;
                 if ($nowMap->get('isEnd')) {
                     //进行替换
-                    $len_s=mb_strlen($str);
-                    $tmp_s='';
-                    for ($j=0;$j<$len_s;$j++) {
-                        $tmp_s.="*";
-                    }
-                    $out=preg_replace("/$str/",$tmp_s,$out,1);
+                    $tmp_s=$this->calCountString($str);
+                    $outString.=$tmp_s;
                     $str = '';
                     $map = $tmp;
                 } else {
@@ -85,11 +81,26 @@ class searchBadWords {
                 //第一次匹配失败，取消上一次匹配，重置进入下一次匹配。
                 if (!empty($str)) {
                     $i--;
-                }   
+                    $outString.=$str;
+                }else{
+                    $outString.=$nowWord;
+                }
                 $str = '';
                 $map = $tmp;
             }
         }
-        return $out;
+        return $outString;
+    }
+    /**
+     * 计算要多少个**
+     * @param type $str
+     */
+    private function calCountString($str) {
+        $len_s=mb_strlen($str);
+        $tmp_s='';
+        for ($j=0;$j<$len_s;$j++) {
+            $tmp_s.="*";
+        }
+        return $tmp_s;
     }
 }
